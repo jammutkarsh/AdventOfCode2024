425b8ee95195199c25e70460d8393b78d59b9c9f
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2025-02-16 19:11:22 +0530

    Initial Project Setup
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent Of Code 2024
+
+Solving Advent of Code 2024 Challenges in Go.
+
+## Usage
+
+```bash
+$ go run main.go 1 # 1 is the day number
+```
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"main/day1"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Day Argument Missing\nUsage: \n\t%s 1\n", os.Args[0])
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "1":
+		day1.Solution()
+	case "2":
+		panic("Implement Me!!!")
+	case "3":
+		panic("Implement Me!!!")
+	case "4":
+		panic("Implement Me!!!")
+	case "5":
+		panic("Implement Me!!!")
+	case "6":
+		panic("Implement Me!!!")
+	case "7":
+		panic("Implement Me!!!")
+	case "8":
+		panic("Implement Me!!!")
+	case "9":
+		panic("Implement Me!!!")
+	case "10":
+		panic("Implement Me!!!")
+	case "11":
+		panic("Implement Me!!!")
+	case "12":
+		panic("Implement Me!!!")
+	case "13":
+		panic("Implement Me!!!")
+	case "14":
+		panic("Implement Me!!!")
+	case "15":
+		panic("Implement Me!!!")
+	case "16":
+		panic("Implement Me!!!")
+	case "17":
+		panic("Implement Me!!!")
+	case "18":
+		panic("Implement Me!!!")
+	case "19":
+		panic("Implement Me!!!")
+	case "20":
+		panic("Implement Me!!!")
+	case "21":
+		panic("Implement Me!!!")
+	case "22":
+		panic("Implement Me!!!")
+	case "23":
+		panic("Implement Me!!!")
+	case "24":
+		panic("Implement Me!!!")
+	case "25":
+		panic("Implement Me!!!")
+	}
+}
